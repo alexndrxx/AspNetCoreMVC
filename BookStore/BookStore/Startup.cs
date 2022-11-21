46097add13244ba3ad13b7321ae79e1085af4881
Aliaksandr Azhgirevich <a.aazhgirevich@mail.ru>
2022-11-21 14:49:26 +0300

    Revert "Added MVC to application"
    This reverts commit a33db26518594494bd8b7781d7c038119827a452.

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -16,7 +16,6 @@ namespace BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,12 +25,15 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-             
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
