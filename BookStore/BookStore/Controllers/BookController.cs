0512b78cd88f1414f3dcc8df2f4eb11bb95f586e
Aliaksandr Azhgirevich <a.aazhgirevich@mail.ru>
2022-11-23 20:45:23 +0300

    Added a new BookController

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"book id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"book with name = {bookName} & author = {authorName}";
+        }
+    }
+}
