0e5df23f5ffaf0f40bf4be780dd5abf913d6924b
Aliaksandr Azhgirevich <a.aazhgirevich@mail.ru>
2022-11-29 12:14:54 +0300

    Added _ViewImports & _ViewStart

--- BookStore/BookStore/Models/BookModel.cs
+++ BookStore/BookStore/Models/BookModel.cs
@@ -9,5 +9,11 @@
         public string Author { get; set; }
 
         public string Description { get; set; }
+
+        public string Language { get; set; }
+
+        public string TotalPages { get; set; }
+
+        public string Category { get; set; }
     }
 }
--- BookStore/BookStore/Repository/BookRepository.cs
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -10,11 +10,11 @@ namespace BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel() { Id = 1, Author = "Boris", Title = "C++", Description = "Some description" },
-                new BookModel() { Id = 2, Author = "Matwee", Title = "C", Description = "Some description" },
-                new BookModel() { Id = 3, Author = "Misha", Title = "Java", Description = "Some description" },
-                new BookModel() { Id = 4, Author = "Kolya", Title = "Pascal", Description = "Some description" },
-                new BookModel() { Id = 5, Author = "Alex", Title = "C#", Description = "Some description" },
+				new BookModel() { Id = 1, Author = "Boris", Title = "C++", Description = "Some description", Language ="English", TotalPages = "1000", Category="Programming"},
+                new BookModel() { Id = 2, Author = "Matwee", Title = "C", Description = "Some description", Language ="English", TotalPages = "1000", Category="Programming" }, 
+                new BookModel() { Id = 3, Author = "Misha", Title = "Java", Description = "Some description", Language ="English", TotalPages = "1000", Category="Programming" },
+                new BookModel() { Id = 4, Author = "Kolya", Title = "Pascal", Description = "Some description", Language ="English", TotalPages = "1000", Category="Programming" },
+                new BookModel() { Id = 5, Author = "Alex", Title = "C#", Description = "Some description", Language ="English", TotalPages = "1000", Category="Programming" },
             };
         }
 
