829ee407ec0de67a94a00d9cf2e7874892e9706e
Aliaksandr Azhgirevich <a.aazhgirevich@mail.ru>
2022-11-29 13:37:36 +0300

    ViewData

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -7,6 +7,9 @@ namespace BookStore.Controllers
 {
     public class BookController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
+
         private readonly BookRepository _repository = null;
 
         public BookController()
@@ -16,12 +19,14 @@ namespace BookStore.Controllers
         public ViewResult GetAllBooks()
         {
             var data = _repository.DataSource();
+            Title = "All books | BookStore";
             return View(data);
         }
 
         public ViewResult GetBook(int id)
         {
             var data = _repository.GetBookById(id);
+            Title = "Book "+data.Title+" | BookStore";
             return View(data);
         }
 
--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -4,18 +4,24 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
+
         public ViewResult Index()
         {
+            Title = "Home | BookStore";
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "AboutUs | BookStore";
             return View();
         }       
         
         public ViewResult ContactUs()
         {
+            Title = "ContactUs | BookStore";
             return View();
         }
 
